5bfac9d80def7744f5508df9103d31c7157790f9
fdricck <s22210431@student.unklab.ac.id>
2024-11-19 21:22:06 +0800

    Adding componenet Header, Main (Intro & Info)

---
+++ src/components/Main/Intro/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div>
+        <section className="cv-block block-intro border-bottom">
+      <div className="container">
+        <div className="avatar">
+          <img className="img-fluid rounded-circle" src="./assets/img/avatar.jpg" />
+        </div>
+        <p>Hello! I am <strong>Fredrick Palangan</strong>.I am a student at the Faculty of Computer Science at
+          Universitas Klabat, majoring in Informatics. I have
+          a strong interest in design and web.</p>
+        <a href="#" className="btn btn-outline-success">Hire Me</a>
+      </div>
+    </section>  
+    </div>
+  );
+}
+;
+export default Intro;
---
+++ src/components/Main/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Intro from './Intro';
+import Info from './Info';
+
+const Main = () => {
+  return (
+    <div>
+        <main className="page cv-page">
+            <Intro/>
+            <Info/>
+        </main>
+    </div>
+  );
+};
+
+export default Main;
